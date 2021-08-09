78766a271027417e5c647d4bfc70bf90107c4c2a
iliaha <83443572+iliaha@users.noreply.github.com>
2021-08-09 19:22:04 +0300

    Update hw.sql

--- hw6/hw.sql
+++ hw6/hw.sql
@@ -3,3 +3,14 @@
 SELECT from_user_id, count(*) AS cnt FROM message WHERE to_user_id = 43 GROUP BY from_user_id ORDER BY cnt desc LIMIT 1;
 
 Задание 3.
+
+SELECT 
+	*
+FROM vk.like 
+WHERE 
+	user_id in (
+    SELECT profile.user_id 
+    FROM vk.profile 
+    ORDER BY TIMESTAMPDIFF(YEAR, birthday, NOW())
+    limit 10
+    );
